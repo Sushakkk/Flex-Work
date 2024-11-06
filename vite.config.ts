fd3110f2c1ad5fc419d4893905cfffbbc658b087
Карпова Ксения <ksenia.karpova.4@mail.ru>
2024-11-06 15:52:27 +0300

    pages public

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-    base: "/Flex-Work",
+    base: "/FlexWork-Frontend",
     server: {
         host: true,
         port: 3000,
